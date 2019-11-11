e81b659f55bee5a7592b187a336757b17a286d14
Touseef Tariq <touseef.tariq@naxxa.io>
2019-11-11 19:03:52 +0500

    api

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return(
-        <div className="ui container" style={{marginTop:"10px"}}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component{
+    onFormSubmit = term => {
+        console.log(term)
+    }
+    render(){
+        return(
+            <div className="ui container" style={{marginTop:"10px"}}>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,18 +5,26 @@ class SearchBar extends React.Component{
     //     console.log(event.target.value);
     // }
 
+    
+
     state = {
         term: ''
     };
 
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         {/* <input onChange={this.onInputChange} /> */}
-                        <input type='text' value={this.state.term} onChange={ e => this.setState({term: e.target.value})} />
+                        <input type='text' value={this.state.term} onChange={ e => this.setState({term: e.target.value })} />
                     </div>
                     
                 </form>
