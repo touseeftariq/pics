f755f81ddc621286f6b2defb815943e7bcd1c22b
Touseef Tariq <touseef.tariq@naxxa.io>
2019-11-18 19:15:30 +0500

    grid perfection

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import { stat } from 'fs';
 
  class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+
+        state = {
+            spans: 0
+        }
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = console.log(this.imageRef.current.clientHeight);
+
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans })
+    }
+
     render() {
+        const {description, urls} = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd : `span ${this.state.spans}` }}>
                 <img
-                    alt={this.props.image.description}
-                    src={this.props.image.url.regular}
+                    ref={this.imageRef}
+                    alt={description}
+                    src={urls.regular}
                 />
             </div>
         )
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,11 @@
 import './imageList.css'
 import React from 'react';
-import imageCard from './ImageCard'
+import ImageCard from './ImageCard'
 
 const ImageList = props => {
-        const images = props.images.map(({id, description, urls}) => {
+        const images = props.images.map(image => {
             // return <img key={id} src={urls.regular} alt={description} />; 
-            return <img key={id} src={urls.regular} alt={description} />; 
+            return <ImageCard key={image.id} image={image} />; 
         });
 
     return <div className='image-list'>{images}</div>;
